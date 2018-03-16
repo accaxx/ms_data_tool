2ec9953e419c939c5ecc1c357f1afe9ad2afa045
aihara005 <asahi.aihara@leverages.jp>
2018-03-16 21:01:51 +0900

    別ファイルの読み込み

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@
 
 <h1><?php
     echo "マスタデータ管理ツール";
+    readfile('../test.php');
 ?>
 </h1>
 </body>
---
+++ test.php
@@ -0,0 +1,9 @@
+'別ファイル'
+<?php
+class First
+{
+    public function __construct()
+    {
+        echo '別ファイルからの読み込み';
+    }
+}
